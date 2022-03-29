846c4b0f4673c2eaf08d3a7fe407c138f8fe5824
Mehrdadkml <mehrdadkml92@gmail.com>
2022-03-30 02:07:42 +0430

    session and views for add to card

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     description=models.TextField(blank=True)
     image=models.ImageField(upload_to='images/')
     slug=models.SlugField(max_length=255,null=True)
-    price=models.DecimalField(max_digits=4,decimal_places=2,null=True)
+    price=models.DecimalField(max_digits=7,decimal_places=3,null=True)
     in_stock=models.BooleanField(default=True)
     is_active=models.BooleanField(default=True)
     created=models.DateTimeField(auto_now_add=True)
